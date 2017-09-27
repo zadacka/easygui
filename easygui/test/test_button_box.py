14d38267a550afff70cc325ae4c784e1a37910f6
Alex Zawadzki <alexander.zawadzki@gmail.com>
2017-09-27 12:43:22 +0200

    Comment out 'manual' tests so that Travis can run

--- easygui/test/test_button_box.py
+++ easygui/test/test_button_box.py
@@ -50,27 +50,27 @@ class TestTextBoxUtilities(unittest.TestCase):
 
 
 class TestButtonBox(unittest.TestCase):
-    # pass
+    pass
 
-    def test_button_box_demo1(self):
-        value = buttonbox(
-            title="First demo",
-            msg="bonjour",
-            choices=["Button[1]", "Button[2]", "Button[3]"],
-            default_choice="Button[2]")
-        print("Return: {}".format(value))
-
-    def test_button_box_demo2(self):
-        package_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)));  # My parent's directory
-        images = list()
-        images.append(os.path.join(package_dir, "python_and_check_logo.gif"))
-        images.append(os.path.join(package_dir, "zzzzz.gif"))
-        images.append(os.path.join(package_dir, "python_and_check_logo.gif"))
-        images = [images, images, images, images, ]
-        value = buttonbox(
-            title="Second demo",
-            msg="Now is a good time to press buttons and show images",
-            choices=['ok', 'cancel'],
-            images=images)
-        print("Return: {}".format(value))
+    # def test_button_box_demo1(self):
+    #     value = buttonbox(
+    #         title="First demo",
+    #         msg="bonjour",
+    #         choices=["Button[1]", "Button[2]", "Button[3]"],
+    #         default_choice="Button[2]")
+    #     print("Return: {}".format(value))
+    #
+    # def test_button_box_demo2(self):
+    #     package_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)));  # My parent's directory
+    #     images = list()
+    #     images.append(os.path.join(package_dir, "python_and_check_logo.gif"))
+    #     images.append(os.path.join(package_dir, "zzzzz.gif"))
+    #     images.append(os.path.join(package_dir, "python_and_check_logo.gif"))
+    #     images = [images, images, images, images, ]
+    #     value = buttonbox(
+    #         title="Second demo",
+    #         msg="Now is a good time to press buttons and show images",
+    #         choices=['ok', 'cancel'],
+    #         images=images)
+    #     print("Return: {}".format(value))
 
